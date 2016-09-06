3b17eadc1414f2a583733013711c5b9bec44985a
Manu Uzkudun <uzkudun@mdpi.com>
2016-09-06 12:26:47 +0200

    Parses a worktime csv file and store records to an array

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express'),
-			app = express();
+			app = express(),
+			csv = require('csv-parser'),
+			fs = require('fs');
 
-app.get('/',(req,res) => {
-	res.send('Hello World');
-});
+var workTimeArray = [];
 
-app.listen(3000,() => {
-	console.log('Example app listening on port 3000!');
-});
+fs.createReadStream('horarios.csv')
+  .pipe(csv({
+		separator: '\t',
+		headers: ['EmployeeId', 'DateTime','ni1', 'ni2','EmployeeName', 'Action','nil3', 'nil4']
+	}))
+  .on('data', (data) => {
+    //TODO: Pass a filtered object to the array
+    console.log(data);
+    workTimeArray.push(data);
+  });
+console.log(workTimeArray);
+// app.get('/',(req,res) => {
+// 	res.send('Hello World');
+// });
+
+// app.listen(3000,() => {
+// 	console.log('Example app listening on port 3000!');
+// });
