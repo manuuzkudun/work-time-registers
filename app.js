817e68f6bd4d5af8d61bf592baeeccef43d2e57b
Manu Uzkudun <uzkudun@mdpi.com>
2016-09-06 15:27:21 +0200

    Root route sends JSON data with the worktimes

--- app.js
+++ app.js
@@ -1,25 +1,38 @@
+'use strict'
+
 const express = require('express'),
 			app = express(),
 			csv = require('csv-parser'),
-			fs = require('fs');
+			fs = require('fs'),
+			dateFormat = require('dateformat');
+
 
 var workTimeArray = [];
 
 fs.createReadStream('horarios.csv')
   .pipe(csv({
 		separator: '\t',
-		headers: ['EmployeeId', 'DateTime','ni1', 'ni2','EmployeeName', 'Action','nil3', 'nil4']
-	}))
-  .on('data', (data) => {
-    //TODO: Pass a filtered object to the array
-    console.log(data);
-    workTimeArray.push(data);
-  });
-console.log(workTimeArray);
-// app.get('/',(req,res) => {
-// 	res.send('Hello World');
-// });
+		headers: ['Id', 'DateTime','ni1', 'ni2','Name', 'Action','nil3', 'nil4']
+	})).on('data', (data) => {
 
-// app.listen(3000,() => {
-// 	console.log('Example app listening on port 3000!');
-// });
+  	let date = new Date(data.DateTime);
+
+  	let dataFiltered = {
+  		Id: data.Id,
+  		Name: data.Name,
+  		Date: dateFormat(date,"dd/mm/yyyy"),
+  		Time: dateFormat(date,"hh:MM:ss"),
+  	};
+    console.log(dataFiltered);
+    workTimeArray.push(dataFiltered);
+  });
+
+
+
+app.get('/',(req,res) => {
+	res.json(workTimeArray);
+});
+
+app.listen(3000,() => {
+	console.log('Example app listening on port 3000!');
+});
