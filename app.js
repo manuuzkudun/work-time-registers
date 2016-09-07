ab2a6b8450beacd8a6bb2016ee77ddef698e186f
Manu Uzkudun <uzkudun@mdpi.com>
2016-09-07 17:21:58 +0200

    angular installed

--- app.js
+++ app.js
@@ -2,26 +2,8 @@
 
 const express = require('express')
   , reload = require('reload')
-  , mysql = require('mysql')
   , bodyParser = require('body-parser')
   , app = express();
-
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'alpha1',
-  database : 'gestion_horas'
-});
-
-connection.connect();
-
-connection.query('SELECT * FROM users', function(err, rows, fields) {
-  if (err) throw err;
-  console.log('The solution is: ', rows[0].email);
-});
-
-connection.end();
 
 // Configure express app
 app.set('port', process.env.PORT || 3000);
@@ -38,7 +20,6 @@ app.use(express.static('./public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/employees'));
 app.use(require('./routes/api'));
-
 
 var server = app.listen(app.get('port'), () => {
   console.log(`Example app listening on port ${app.get('port')}!`);
--- dataManagement.js
+++ dataManagement.js
@@ -1,31 +1,48 @@
-'use strict'
+'use strict';
 
-const	csv = require('csv-parser'),
-			fs = require('fs'),
-			dateFormat = require('dateformat'),
-			actions = {'I': 'start', 'O': 'leave', '0': 'start-rest', '1': 'end-rest' };
+const csv = require('csv-parser')
+  , fs = require('fs')
+  , dateFormat = require('dateformat')
+  , mysql = require('mysql')
+  , actions = {
+    'I': 'start'
+    , 'O': 'leave'
+    , '0': 'start-rest'
+    , '1': 'end-rest'
+  };
 
+var connection = mysql.createConnection({
+  host: 'localhost'
+  , user: 'root'
+  , password: 'alpha1'
+  , database: 'gestion_horas'
+});
 
+connection.connect();
+
+connection.query('SELECT * FROM users', function (err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].email);
+});
+
+connection.end();
 
 var workTimeArray = [];
 
-fs.createReadStream('records.csv')
-  .pipe(csv({
-		separator: '\t',
-		headers: ['Id', 'DateTime','ni1', 'ni2','Name', 'Action','nil3', 'nil4']
-	})).on('data', (data) => {
+fs.createReadStream('records.csv').pipe(csv({
+  separator: '\t'
+  , headers: ['Id', 'DateTime', 'ni1', 'ni2', 'Name', 'Action', 'nil3', 'nil4']
+})).on('data', (data) => {
+  let date = new Date(data.DateTime);
+  let dataFiltered = {
+    Id: data.Id
+    , Name: data.Name
+    , Date: dateFormat(date, "dd/mm/yyyy")
+    , Time: dateFormat(date, "hh:MM:ss")
+    , Action: actions[data.Action]
+  };
+  workTimeArray.push(dataFiltered);
+});
 
-  	let date = new Date(data.DateTime);
 
-  	let dataFiltered = {
-  		Id: data.Id,
-  		Name: data.Name,
-  		Date: dateFormat(date,"dd/mm/yyyy"),
-  		Time: dateFormat(date,"hh:MM:ss"),
-  		Action: actions[data.Action]
-  	};
-
-    workTimeArray.push(dataFiltered);
-  });
-
- module.exports = workTimeArray;
+module.exports = workTimeArray;
