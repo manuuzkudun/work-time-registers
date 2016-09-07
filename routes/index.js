8cdf875d49b62fd2f4991d4eec54f8676bc6710e
Manu Uzkudun <uzkudun@mdpi.com>
2016-09-07 10:28:15 +0200

    Cleans old code

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,8 @@ const express = require('express'),
 			router = express.Router();
 
 router.get('/', (req,res) => {
-// 	res.send(`Hello World!!!
-// 		<script src="/reload/reload.js"></script>`);
-res.render('home');
+	res.render('home');
 });
-
-
-
 
 module.exports= router;
 
