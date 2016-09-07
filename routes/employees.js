0d082b0eaf074874b43542cfef7bf6b9fd452246
Manu Uzkudun <uzkudun@mdpi.com>
2016-09-07 10:10:40 +0200

    worktime data appears in bootstrap tables

--- routes/employees.js
+++ routes/employees.js
@@ -10,44 +10,47 @@ var workTimeArray = require('../dataManagement');
 
 
 router.get('/employees',(req,res) => {
-	
+	res.render('worktime', {records:workTimeArray});
 	//res.json(workTimeArray);
-	var info = '<ul>';
-	workTimeArray.forEach((record) => {
-		info += `
-		<li>
-			<h2>${record.Name}</h2>
-			<p>${record.Date}</p>
-			<p>${record.Time}</p>
-			<p>${record.Action}</p>
-		</li>
-	`
-	});
-	info += '</ul>'
-	info += '<script src="/reload/reload.js"></script>'
-	res.send(info);
+	// var info = '<ul>';
+	// workTimeArray.forEach((record) => {
+	// 	info += `
+	// 	<li>
+	// 		<h2>${record.Name}</h2>
+	// 		<p>${record.Date}</p>
+	// 		<p>${record.Time}</p>
+	// 		<p>${record.Action}</p>
+	// 	</li>
+	// `
+	// });
+	// info += '</ul>'
+	// info += '<script src="/reload/reload.js"></script>'
+	// res.send(info);
 });
 
 router.get('/employee/:id',(req,res) => {
 	var filtered = _.where(workTimeArray, {Id: req.params.id});
-	if ( _.isEmpty(filtered) )
-			res.send(`Sorry, no record for Id: ${req.params.id}`);
-	else {
-		var info = '<ul>';
-	filtered.forEach((record) => {
-		info += `
-		<li>
-			<h2>${record.Name}</h2>
-			<p>${record.Date}</p>
-			<p>${record.Time}</p>
-			<p>${record.Action}</p>
-		</li>
-	`
-	});
-	info += '</ul>'
-	info += '<script src="/reload/reload.js"></script>'
-	res.send(info);
-	}
+
+	res.render('worktime', {records:filtered});
+	// var filtered = _.where(workTimeArray, {Id: req.params.id});
+	// if ( _.isEmpty(filtered) )
+	// 		res.send(`Sorry, no record for Id: ${req.params.id}`);
+	// else {
+	// 	var info = '<ul>';
+	// filtered.forEach((record) => {
+	// 	info += `
+	// 	<li>
+	// 		<h2>${record.Name}</h2>
+	// 		<p>${record.Date}</p>
+	// 		<p>${record.Time}</p>
+	// 		<p>${record.Action}</p>
+	// 	</li>
+	// `
+	// });
+	// info += '</ul>'
+	// info += '<script src="/reload/reload.js"></script>'
+	// res.send(info);
+	// }
 });
 
 module.exports = router;
