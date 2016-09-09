ccac8d2e9b1cbb851fe6907914085ccb1511b045
Manu Uzkudun <uzkudun@mdpi.com>
2016-09-09 09:46:08 +0200

    Installed mongodb and configured the connection

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
 const express = require('express')
   , reload = require('reload')
   , bodyParser = require('body-parser')
+  , mongo = require('mongodb').MongoClient
   , app = express();
+
+var database;
 
 // Configure express app
 app.set('port', process.env.PORT || 3000);
@@ -26,8 +29,13 @@ app.use(require('./routes/index'));
 app.use(require('./routes/employees'));
 app.use(require('./routes/api'));
 
-
-
+// Connect to mongodb and set the database connection
+mongo.connect("mongodb://localhost:27017/test", (err,db) =>{
+  if(!err){
+    console.log("We are connected to mongoDb");
+    database = db;
+  }
+});
 
 var server = app.listen(app.get('port'), () => {
   console.log(`Example app listening on port ${app.get('port')}!`);
