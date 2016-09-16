a841801ca8d17a3e0ceb711a5b0a14840d15fc45
Manu Uzkudun <uzkudun@mdpi.com>
2016-09-16 13:22:31 +0200

    /employees/names associated API implemented

--- routes/api.js
+++ routes/api.js
@@ -7,33 +7,38 @@ const express = require('express'),
 
 
 router.get('/employee/:id', (req,res) =>{
-  Employee.findById({_id: req.params.id}).exec( (err,result) => {
+  Employee.findById({_id: req.params.id}, (err,result) => {
     if (err) throw err;
     if (result) {
-      // Get registers from employee
-      let registers = result.registers.map(reg => {
-        return {
-          dateTime: reg.dateTime,
-          date: dateFormat(reg.dateTime,"dd/mm/yyyy"),
-          time: dateFormat(reg.dateTime,"HH:MM:ss"),
-          action: reg.action
-        }
-      });
-      // Send JSON response
-      res.json({
+      let vm = {
         msg: 'success',
-        id: result._id,
-        name: result.name,
-        email: result.email,
-        registers: registers
-      });
-    }
-    else {
+        data: {
+          id: result._id,
+          name: result.name,
+          email: result.email,
+          registers: result.registers.map(reg => { return {
+            dateTime: reg.dateTime,
+            date: dateFormat(reg.dateTime,"dd/mm/yyyy"),
+            time: dateFormat(reg.dateTime,"HH:MM:ss"),
+            action: reg.action
+          }
+        })
+      }
+    };
+    res.json(vm);
+  } else {
       res.json({msg: 'no-data'});
     }
   });
 });
 
 
+
+router.get('/employees/names', (req,res) => {
+  Employee.find({}, '_id name', (err,results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
 
 module.exports = router;
