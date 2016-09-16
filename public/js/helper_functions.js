bd7901ffe9e8bba6323d8195ba5d4afda2a17d73
Manu Uzkudun <uzkudun@mdpi.com>
2016-09-16 16:02:13 +0200

    Employee photo placeholder added and raw data shows correctly

---
+++ public/js/helper_functions.js
@@ -0,0 +1,29 @@
+/*  function timeDifference(dateTime1, dateTime2) {
+    var diff = new Date(dateTime2) - new Date(dateTime1);
+    var seconds = diff / 1000;
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+    hours = hours - (days * 24);
+    minutes = minutes - (days * 24 * 60) - (hours * 60);
+    seconds = seconds - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
+    return hours + ":" + minutes + ":" + seconds;
+  }*/
+
+
+  function computeTotalWork(startWork,leaveWork){
+    if (startWork.dateTime && leaveWork.dateTime){
+      return timeDifference(startWork.dateTime, leaveWork.dateTime);
+    } else {
+      return 'NO-DATA';
+    }
+  }
+  
+  function computeTotalRest(startRest,endRest){
+    if ((startRest.length >0)  && (endRest.length > 0) && (startRest.length == endRest.length)) {
+         return timeDifference(startRest[0].dateTime,endRest[0].dateTime);
+    } else {
+      return 'NO_DATA';
+    }
+    
+  }
--- public/js/time_data.js
+++ public/js/time_data.js
@@ -1,47 +1,23 @@
-
-
 var timeData = angular.module("timeData",[]);
 
-
-timeData.controller('myController', function ($scope, $http) {
-  
+timeData.controller('myController', function ($scope, $http) {  
   $scope.data = null;
   $scope.dataProcessed = null;
-  getEmployeeNames();
+  getEmployees();
 
   
   $scope.sortComment = function(comment) {
     var date = new Date(comment.dateTime);
     return date;
 };
-    
-  function getProps(data, prop) {
-    var props = data.map(function (obj) {
-      return obj[prop];
-    });
-    props = props.filter(function (v, i) {
-      return props.indexOf(v) == i;
-    });
-    return props;
-  }
-
 
   function timeDifference(dateTime1,dateTime2){
     var start = new Date(dateTime1);
     var end = new Date(dateTime2);
     return countdown(start,end); 
   }
-  
-  function getDate(dateTimeString){
-    var dateTime = new Date(dateTimeString);
-    var day = dateTime.getDate(); 
-    var month = date.getMonth(); 
-    var year = date.getFullYear();
-    var date = `${day}/${month}/${year}`
-    return date;
-  }
-  
-  function getEmployeeNames() {
+    
+  function getEmployees() {
     $http.get('http://localhost:3000/api/employees/names').success(function (data) {
       $scope.employees = data; 
     });
@@ -55,40 +31,24 @@ timeData.controller('myController', function ($scope, $http) {
     return out;
   }
   
-  function computeTotalWork(startWork,leaveWork){
-    if (startWork.dateTime && leaveWork.dateTime){
-      return timeDifference(startWork.dateTime, leaveWork.dateTime);
-    } else {
-      return 'NO-DATA';
-    }
-  }
-  
-  function computeTotalRest(startRest,endRest){
-    if ((startRest.length >0)  && (endRest.length > 0) && (startRest.length == endRest.length)) {
-         return timeDifference(startRest[0].dateTime,endRest[0].dateTime);
-    } else {
-      return 'NO_DATA';
-    }
-    
-  }
-  
-  
-/*  $scope.getEmployeeData = function () {
-
-      
-    var url = 'http://localhost:3000/api/registers/1';
+  $scope.getEmployeeData = function () {  
+    var url = 'http://localhost:3000/api/employee/1';
     $http.get(url).then(function (response) {
-        var data = response.data;
-       $scope.data = data;
-      
-            var dates = _.unique(data,function(record){
+      var data = response.data.data;
+      $scope.registers = data.registers;
+      $scope.name = data.name;
+      $scope.email = data.email;
+      console.log(data.registers[0]);
+    });
+  };
+/*            var dates = _.unique(data,function(record){
         return moment(record.dateTime).format("DD/MM/YYYY");
       }).map(function(record){
         return moment(record.dateTime).format("DD/MM/YYYY");
-      });
+      });*/
       
 
-      $scope.dataProcessed = dates.map(function (date) {
+/*      $scope.dataProcessed = dates.map(function (date) {
 
         var dayRecords = data.filter(function(record){
           return(moment(record.dateTime).format("DD/MM/YYYY") == date);
@@ -109,8 +69,6 @@ timeData.controller('myController', function ($scope, $http) {
           , totalRest: totalRest.toString()
           , totalWork: totalWork
         });
-      });
+      });*/
 
-  });
-  }*/
 });
