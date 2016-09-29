7fb21dd2001effc41eaf96940fda0a03851f1735
Manu Uzkudun <uzkudun@mdpi.com>
2016-09-29 12:07:41 +0200

    Changed db to external

--- config/db_connect.js
+++ config/db_connect.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose'),
       config = require('./db_config');
 
-mongoose.connect(config.local_db_uri, (err, db) => {
+mongoose.connect(config.ext_db_uri, (err, db) => {
   if (!err) {
     console.log("We are connected to mongoDb");
   }
